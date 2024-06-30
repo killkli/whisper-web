bb8b80f1613c22f973c1521a29fca275ae3e2a6f
killkli <dchensterebay@gmail.com>
2024-06-30 17:33:01 +0800

    update for Video Player and making vtt subtitle

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,29 +1,61 @@
 import { useEffect, useRef } from "react";
+import { convertToVTT } from "../utils/AudioUtils"
+import { TranscriberData } from "../hooks/useTranscriber"
 
 export default function AudioPlayer(props: {
-    audioUrl: string;
-    mimeType: string;
+  audioUrl: string;
+  mimeType: string;
+  transcribedData: TranscriberData
 }) {
-    const audioPlayer = useRef<HTMLAudioElement>(null);
-    const audioSource = useRef<HTMLSourceElement>(null);
+  const audioPlayer = useRef<HTMLAudioElement>(null);
+  const audioSource = useRef<HTMLSourceElement>(null);
+  const videoPlayer = useRef<HTMLVideoElement>(null);
+  const videoSource = useRef<HTMLSourceElement>(null);
+  const subtitleSource = useRef<HTMLTrackElement>(null);
 
-    // Updates src when url changes
-    useEffect(() => {
-        if (audioPlayer.current && audioSource.current) {
-            audioSource.current.src = props.audioUrl;
-            audioPlayer.current.load();
-        }
-    }, [props.audioUrl]);
+  // Updates src when url changes
+  useEffect(() => {
+    if (audioPlayer.current && audioSource.current) {
+      audioSource.current.src = props.audioUrl;
+      audioPlayer.current.load();
+    }
+    if (videoPlayer.current && videoSource.current) {
+      videoSource.current.src = props.audioUrl;
+      videoPlayer.current.load();
+    }
+  }, [props.audioUrl]);
 
-    return (
-        <div className='flex relative z-10 p-4 w-full mt-4'>
-            <audio
-                ref={audioPlayer}
-                controls
-                className='w-full h-14 rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10'
-            >
-                <source ref={audioSource} type={props.mimeType}></source>
-            </audio>
-        </div>
-    );
+  useEffect(() => {
+    if (props.transcribedData && !props.transcribedData.isBusy) {
+      if (subtitleSource.current) {
+        const vttText = convertToVTT(props.transcribedData.chunks)
+        const vttBlob = new Blob([vttText], { type: "text/vtt" });
+        const vttUrl = URL.createObjectURL(vttBlob);
+        subtitleSource.current.src = vttUrl;
+      }
+    }
+  }, [props.transcribedData])
+
+  return (
+    <div className='flex relative z-10 p-4 w-full'>
+      {props.mimeType.includes("video") ? (
+        <video
+          ref={videoPlayer}
+          controls
+          className='w-full rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10'
+        >
+          <source ref={videoSource} type={props.mimeType}></source>
+          <track ref={subtitleSource} kind="subtitles" />
+        </video>
+      ) : (
+        <audio
+          ref={audioPlayer}
+          controls
+          className='w-full h-14 rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10'
+        >
+          <source ref={audioSource} type={props.mimeType}></source>
+        </audio>
+      )}
+    </div>
+  );
 }
--- src/utils/AudioUtils.ts
+++ src/utils/AudioUtils.ts
@@ -1,14 +1,35 @@
 function padTime(time: number) {
-    return String(time).padStart(2, "0");
+  return String(time).padStart(2, "0");
 }
 
 export function formatAudioTimestamp(time: number) {
-    const hours = (time / (60 * 60)) | 0;
-    time -= hours * (60 * 60);
-    const minutes = (time / 60) | 0;
-    time -= minutes * 60;
-    const seconds = time | 0;
-    return `${hours ? padTime(hours) + ":" : ""}${padTime(minutes)}:${padTime(
-        seconds,
-    )}`;
+  const hours = (time / (60 * 60)) | 0;
+  time -= hours * (60 * 60);
+  const minutes = (time / 60) | 0;
+  time -= minutes * 60;
+  const seconds = time | 0;
+  return `${hours ? padTime(hours) + ":" : ""}${padTime(minutes)}:${padTime(
+    seconds,
+  )}`;
 }
+
+const formatTimestampVTT = (seconds: number) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const secs = (seconds % 60).toFixed(3);
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secs.padStart(6, '0')}`;
+};
+
+// Function to convert JSON to VTT format
+export const convertToVTT = (data: Array<{
+  timestamp: [number, number | null];
+  text: string;
+}>) => {
+  const subtitles = data.map((item, index) => {
+    const startTime = formatTimestampVTT(item.timestamp[0]);
+    const endTime = formatTimestampVTT(item.timestamp[1] ?? 0);
+    return `${index + 1}\n${startTime} --> ${endTime}\n${item.text}\n`;
+  }).join('\n');
+  const initVTT = 'WEBVTT\n\n';
+  return initVTT + subtitles;
+};
