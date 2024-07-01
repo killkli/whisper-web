a870c6f58f3e064673c314352e1440a36e489983
killkli <dchensterebay@gmail.com>
2024-07-01 11:23:49 +0800

    For gernal purpos

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,29 +1,120 @@
 import { useEffect, useRef } from "react";
+import { convertToVTT } from "../utils/AudioUtils"
+import { TranscriberData } from "../hooks/useTranscriber"
 
 export default function AudioPlayer(props: {
-    audioUrl: string;
-    mimeType: string;
+  audioUrl: string;
+  mimeType: string;
+  transcribedData?: TranscriberData
 }) {
-    const audioPlayer = useRef<HTMLAudioElement>(null);
-    const audioSource = useRef<HTMLSourceElement>(null);
+  const audioPlayer = useRef<HTMLAudioElement>(null);
+  const audioSource = useRef<HTMLSourceElement>(null);
+  const videoPlayer = useRef<HTMLVideoElement>(null);
+  const videoSource = useRef<HTMLSourceElement>(null);
+  const subtitleSource = useRef<HTMLTrackElement>(null);
+  const linkTarget = useRef<HTMLButtonElement>(null);
 
-    // Updates src when url changes
-    useEffect(() => {
-        if (audioPlayer.current && audioSource.current) {
-            audioSource.current.src = props.audioUrl;
-            audioPlayer.current.load();
+  const playtime_event = (event: Event) => {
+    if (!props.transcribedData) return;
+    const chunks = props.transcribedData.chunks
+    const element = event.target as HTMLMediaElement
+    const currentTime = element.currentTime
+    let targetIdx = -1;
+    for (let i = 0; i < chunks.length; i++) {
+      const c = chunks[i]
+      if (c.timestamp[0] <= currentTime && (c.timestamp[1] ?? 0) >= currentTime) {
+        targetIdx = i
+        break
+      }
+    }
+    if (targetIdx === -1) return;
+    const query = `[data-testid="chunk-${targetIdx}"]`;
+    const target = document.querySelector(query)
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" })
+      target.classList.add("bg-yellow-300/50")
+    }
+  }
+  const clear_event = () => {
+    const clearElements = document.querySelectorAll("[data-testid]")
+    clearElements.forEach(e => {
+      e.classList.remove("bg-yellow-300/50")
+    });
+  }
+
+
+  // Updates src when url changes
+  useEffect(() => {
+    if (audioPlayer.current && audioSource.current) {
+      audioSource.current.src = props.audioUrl;
+      audioPlayer.current.load();
+      audioPlayer.current.ontimeupdate = playtime_event;
+      audioPlayer.current.onpause = clear_event;
+      audioPlayer.current.onplay = clear_event;
+    }
+    if (videoPlayer.current && videoSource.current) {
+      videoSource.current.src = props.audioUrl;
+      videoPlayer.current.load();
+      videoPlayer.current.ontimeupdate = playtime_event;
+      videoPlayer.current.onpause = clear_event;
+      videoPlayer.current.onplay = clear_event;
+    }
+  }, [props.audioUrl]);
+
+  useEffect(() => {
+    if (props.transcribedData && !props.transcribedData.isBusy) {
+      const vttText = convertToVTT(props.transcribedData.chunks)
+      const vttBlob = new Blob([vttText], { type: "text/vtt" });
+      const vttUrl = URL.createObjectURL(vttBlob);
+      if (subtitleSource.current) {
+        subtitleSource.current.src = vttUrl;
+      }
+      if (linkTarget.current) {
+        linkTarget.current.onclick = () => {
+          const link = document.createElement("a");
+          link.href = vttUrl;
+          link.download = "subtitle.vtt";
+          link.click();
         }
-    }, [props.audioUrl]);
+      }
+    }
+    if (audioPlayer.current) {
+      audioPlayer.current.ontimeupdate = playtime_event;
+      audioPlayer.current.onpause = clear_event;
+      audioPlayer.current.onplay = clear_event;
+    }
+    if (videoPlayer.current) {
+      videoPlayer.current.ontimeupdate = playtime_event;
+      videoPlayer.current.onpause = clear_event;
+      videoPlayer.current.onplay = clear_event;
+    }
+  }, [props.transcribedData])
 
-    return (
-        <div className='flex relative z-10 p-4 w-full'>
-            <audio
-                ref={audioPlayer}
-                controls
-                className='w-full h-14 rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10'
-            >
-                <source ref={audioSource} type={props.mimeType}></source>
-            </audio>
-        </div>
-    );
+  return (
+    <div className='flex relative z-10 p-4 w-full'>
+      {props.mimeType.includes("video") ? (
+        <video
+          ref={videoPlayer}
+          controls
+          className='w-full rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10'
+        >
+          <source ref={videoSource} type={props.mimeType}></source>
+          <track ref={subtitleSource} kind="subtitles" default srcLang="zh" />
+        </video>
+      ) : (
+        <audio
+          ref={audioPlayer}
+          controls
+          className='w-full h-14 rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10'
+        >
+          <source ref={audioSource} type={props.mimeType}></source>
+        </audio>
+      )}
+      {
+        props.transcribedData?.isBusy !== true && (
+          <button ref={linkTarget} className="bg-slate-500/50 text-white p-2 rounded-xl absolute right-4 top-4">字幕下載</button>
+        )
+      }
+    </div>
+  );
 }
--- src/components/Transcript.tsx
+++ src/components/Transcript.tsx
@@ -1,93 +1,226 @@
-import { useRef, useEffect } from "react";
-
-import { TranscriberData } from "../hooks/useTranscriber";
+import { useRef, useEffect, useState } from "react";
+import { Transcriber, TranscriberData } from "../hooks/useTranscriber";
 import { formatAudioTimestamp } from "../utils/AudioUtils";
 
 interface Props {
-    transcribedData: TranscriberData | undefined;
+  transcribedData: TranscriberData | undefined;
+  transcriber: Transcriber
 }
 
-export default function Transcript({ transcribedData }: Props) {
-    const divRef = useRef<HTMLDivElement>(null);
+export default function Transcript({ transcribedData, transcriber }: Props) {
+  const divRef = useRef<HTMLDivElement>(null);
 
-    const saveBlob = (blob: Blob, filename: string) => {
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = filename;
-        link.click();
-        URL.revokeObjectURL(url);
-    };
-    const exportTXT = () => {
-        let chunks = transcribedData?.chunks ?? [];
-        let text = chunks
-            .map((chunk) => chunk.text)
-            .join("")
-            .trim();
+  const saveBlob = (blob: Blob, filename: string) => {
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = filename;
+    link.click();
+    URL.revokeObjectURL(url);
+  };
+  const exportTXT = () => {
+    let chunks = transcribedData?.chunks ?? [];
+    let text = chunks
+      .map((chunk) => chunk.text)
+      .join("")
+      .trim();
 
-        const blob = new Blob([text], { type: "text/plain" });
-        saveBlob(blob, "transcript.txt");
-    };
-    const exportJSON = () => {
-        let jsonData = JSON.stringify(transcribedData?.chunks ?? [], null, 2);
+    const blob = new Blob([text], { type: "text/plain" });
+    saveBlob(blob, "transcript.txt");
+  };
+  const exportJSON = () => {
+    let jsonData = JSON.stringify(transcribedData?.chunks ?? [], null, 2);
 
-        // post-process the JSON to make it more readable
-        const regex = /(    "timestamp": )\[\s+(\S+)\s+(\S+)\s+\]/gm;
-        jsonData = jsonData.replace(regex, "$1[$2 $3]");
+    // post-process the JSON to make it more readable
+    const regex = /(    "timestamp": )\[\s+(\S+)\s+(\S+)\s+\]/gm;
+    jsonData = jsonData.replace(regex, "$1[$2 $3]");
 
-        const blob = new Blob([jsonData], { type: "application/json" });
-        saveBlob(blob, "transcript.json");
-    };
+    const blob = new Blob([jsonData], { type: "application/json" });
+    saveBlob(blob, "transcript.json");
+  };
 
-    // Scroll to the bottom when the component updates
-    useEffect(() => {
-        if (divRef.current) {
-            const diff = Math.abs(
-                divRef.current.offsetHeight +
-                    divRef.current.scrollTop -
-                    divRef.current.scrollHeight,
-            );
+  // Scroll to the bottom when the component updates
+  useEffect(() => {
+    if (divRef.current) {
+      const diff = Math.abs(
+        divRef.current.offsetHeight +
+        divRef.current.scrollTop -
+        divRef.current.scrollHeight,
+      );
 
-            if (diff <= 64) {
-                // We're close enough to the bottom, so scroll to the bottom
-                divRef.current.scrollTop = divRef.current.scrollHeight;
-            }
-        }
-    });
+      if (diff <= 100) {
+        // We're close enough to the bottom, so scroll to the bottom
+        divRef.current.scrollTop = divRef.current.scrollHeight;
+      }
+    }
+  });
 
-    return (
-        <div
-            ref={divRef}
-            className='w-full flex flex-col my-2 p-4 max-h-[20rem] overflow-y-auto'
-        >
-            {transcribedData?.chunks &&
-                transcribedData.chunks.map((chunk, i) => (
-                    <div
-                        key={`${i}-${chunk.text}`}
-                        className='w-full flex flex-row mb-2 bg-white rounded-lg p-4 shadow-xl shadow-black/5 ring-1 ring-slate-700/10'
-                    >
-                        <div className='mr-5'>
-                            {formatAudioTimestamp(chunk.timestamp[0])}
-                        </div>
-                        {chunk.text}
-                    </div>
-                ))}
-            {transcribedData && !transcribedData.isBusy && (
-                <div className='w-full text-right'>
-                    <button
-                        onClick={exportTXT}
-                        className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 inline-flex items-center'
-                    >
-                        Export TXT
-                    </button>
-                    <button
-                        onClick={exportJSON}
-                        className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 inline-flex items-center'
-                    >
-                        Export JSON
-                    </button>
+  const videoJumpTo = (time: number) => {
+    const video = document.querySelector('video');
+    if (video) {
+      video.currentTime = time - 0.1;
+    }
+    const audio = document.querySelector('audio');
+    if (audio) {
+      audio.currentTime = time - 0.1;
+    }
+  }
+
+  const [currentEditingIndex, setCurrentEditingIndex] = useState<number | null>(null);
+  const [editingContent, setEditingContent] = useState<string>("");
+  const saveEditingContent = () => {
+    if (!transcribedData || !transcribedData.chunks || currentEditingIndex === null) {
+      setCurrentEditingIndex(null);
+      setEditingContent("");
+      return;
+    }
+    const newData = structuredClone(transcribedData);
+    newData.chunks[currentEditingIndex].text = editingContent;
+    transcriber.setTranscript(newData);
+    setCurrentEditingIndex(null);
+    setEditingContent("");
+  }
+
+  const [cEditTimeIdx, setCETI] = useState<number | null>(null);
+  const [cEditTime, setCET] = useState<[number, number | null]>([0, 0]);
+  const setCETIAndCET = () => {
+    if (!transcribedData || !transcribedData.chunks || cEditTimeIdx === null) {
+      setCETI(null);
+      setCET([0, 0]);
+      return;
+    }
+    const newData = structuredClone(transcribedData);
+    newData.chunks[cEditTimeIdx].timestamp = [cEditTime[0], cEditTime[1]];
+    transcriber.setTranscript(newData);
+    setCETI(null);
+    setCET([0, 0]);
+  }
+
+  const mergeChunk = (i: number, j: number) => {
+    if (!transcribedData || !transcribedData.chunks) {
+      return
+    }
+    const newData = structuredClone(transcribedData);
+    newData.chunks[i].text += newData.chunks[j].text;
+    newData.chunks[i].timestamp[1] = newData.chunks[j].timestamp[1];
+    newData.chunks.splice(j, 1);
+    transcriber.setTranscript(newData);
+  }
+
+  const addChunk = (i: number) => {
+    if (!transcribedData || !transcribedData.chunks) {
+      return
+    }
+    const newData = structuredClone(transcribedData);
+    const newChunk = {
+      text: newData.chunks[i].text,
+      timestamp: [newData.chunks[i].timestamp[0], newData.chunks[i].timestamp[1]]
+    }
+    if (i < newData.chunks.length - 1) {
+      newData.chunks.splice(i + 1, 0, newChunk);
+    } else {
+      newData.chunks.push(newChunk);
+    }
+    transcriber.setTranscript(newData);
+  }
+  const removeChunk = (i: number) => {
+    if (!transcribedData || !transcribedData.chunks) {
+      return
+    }
+    const newData = structuredClone(transcribedData);
+    if (i < newData.chunks.length - 1) {
+      newData.chunks.splice(i, 1);
+    } else {
+      newData.chunks.pop()
+    }
+    transcriber.setTranscript(newData);
+  }
+
+
+
+
+
+
+  return (
+    <div
+      ref={divRef}
+      className='w-[30rem] flex flex-col my-2 p-4 max-h-[80vh] overflow-y-auto'
+    >
+      {transcribedData?.chunks &&
+        transcribedData.chunks.map((chunk, i) => (
+          <>
+            <div
+              data-testid={`chunk-${i}`}
+              key={`${i}-${chunk.text}`}
+              className={`group w-full relative flex flex-row mb-2 ${transcribedData?.isBusy ? 'bg-gray-100' : 'bg-white'} hover:bg-gray-200 rounded-lg p-4 shadow-xl shadow-black/5 ring-1 ring-slate-700/10`}
+              onClick={() => videoJumpTo(chunk.timestamp[0])}
+            >
+              {cEditTimeIdx === i ? (
+                <div className='mr-5'>
+                  <input className='w-full bg-gray-100 rounded-lg p-2 mx-1' value={cEditTime[0]} onChange={(e) => setCET([Number(e.target.value), cEditTime[1]])} />
+                  <input className='w-full bg-gray-100 rounded-lg p-2 mx-1' value={cEditTime[1] ?? 0} onChange={(e) => setCET([cEditTime[0], Number(e.target.value)])} />
+                  <button className="mx-1 bg-gray-100 text-gray-500 hover:bg-gray-200 focus:ring-2 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center" onClick={setCETIAndCET}>Save</button>
                 </div>
+              ) : (
+                <div className='mr-5'
+                  onDoubleClick={() => {
+                    setCETI(i);
+                    setCET([...chunk.timestamp])
+                  }}
+                >
+                  {formatAudioTimestamp(chunk.timestamp[0])} -
+                  {formatAudioTimestamp(chunk.timestamp[1])}
+                </div>
+              )}
+              {currentEditingIndex === i ? (
+                <div className="flex flex-row flex-wrap w-full">
+                  <textarea rows={4} className='w-full bg-gray-100 rounded-lg p-2 mx-1' value={editingContent} onChange={(e) => setEditingContent(e.target.value)} />
+                  <button className="m-1 bg-gray-100 text-gray-500 hover:bg-gray-200 focus:ring-2 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center" onClick={saveEditingContent}>Save</button>
+                  <button className="m-1 bg-gray-100 text-gray-500 hover:bg-gray-200 focus:ring-2 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center" onClick={() => setCurrentEditingIndex(null)}>取消</button>
+                </div>
+              ) : (
+                <span
+                  onDoubleClick={() => {
+                    setCurrentEditingIndex(i);
+                    setEditingContent(chunk.text)
+                  }}
+                  className="text-gray-500">{chunk.text}</span>
+              )}
+              <div className="absolute bg-amber-500 text-white rounded-full px-2 py-1 top-1 right-1 invisible group-hover:visible hover:cursor-pointer hover:text-white hover:bg-gray-500" onClick={() => removeChunk(i)}>X</div>
+            </div>
+            {i < transcribedData.chunks.length && (
+              <div className='w-full h-1 bg-gray-300 my-2 text-center text-gray-500/0 hover:h-[2rem] hover:text-gray-500 ' >
+                {i < transcribedData.chunks.length - 1 && <span className="mx-4 hover:cursor-pointer hover:text-white hover:bg-gray-500" onClick={() => mergeChunk(i, i + 1)}>合</span>}
+                <span className="mx-4 hover:cursor-pointer hover:text-white hover:bg-gray-500" onClick={() => addChunk(i)}>+</span>
+                <span className="mx-4 hover:cursor-pointer hover:text-white hover:bg-gray-500"
+                  onClick={() => {
+                    setCurrentEditingIndex(i);
+                    setEditingContent(chunk.text)
+                  }}
+                >編輯</span>
+              </div>
             )}
-        </div>
-    );
+          </>
+        ))
+      }
+      {
+        transcribedData && !transcribedData.isBusy && (
+          <div className='w-full text-right'>
+            <button
+              onClick={exportTXT}
+              className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 inline-flex items-center'
+            >
+              Export TXT
+            </button>
+            <button
+              onClick={exportJSON}
+              className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 inline-flex items-center'
+            >
+              Export JSON
+            </button>
+          </div>
+        )
+      }
+    </div >
+  );
 }
